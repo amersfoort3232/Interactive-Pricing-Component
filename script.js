ea2695c10779e21870037b17bd5fbcfae9a62fd4
Amersfoort Lam <amersfoortlam@gmail.com>
2024-03-21 13:20:19 +0000

    Added discount price

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const values = ["$8.00", "$12.00", "$16.00", "$24.00", "$36.00"];
 const pages = ["10K", "50K", "100K", "500K", "1M"];
+const discount_value = ["6", "9", " 12", "18", "27"];
 const input = document.getElementById("input"),
   output = document.getElementById("output"),
   pageoutput = document.getElementById("pageoutput");
 
 input.oninput = function () {
-  output.innerHTML = values[this.value];
-  pageoutput.innerHTML = pages[this.value];
-  console.log(values[this.value]);
-  console.log(pages[this.value]);
+  if (document.querySelector(".discount-rate").checked == true) {
+    pageoutput.innerHTML = discount_value[this.value];
+  } else {
+    output.innerHTML = values[this.value];
+    pageoutput.innerHTML = pages[this.value];
+  }
 };
 
 // set the default value
